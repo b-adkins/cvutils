71d5108f24164c5cb1acafb5c4dd1780c4502909
Boone Adkins <boone.adkins@gmail.com>
2016-01-31 14:40:56 -0600

    A utility to make manual classification quicker.

---
+++ quick_classify.py
@@ -0,0 +1,83 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as pyplot
+import os
+import sys
+
+class ImageClassifierGUI():
+    def __init__(self, key_to_dir, files):
+        self.image_itr = files.__iter__()
+        self.current_image = self.image_itr.next()
+
+        self.fig, self.ax = pyplot.subplots()
+
+        self.fig.canvas.mpl_connect('key_press_event', self.handle_keypress)
+        img = mpimg.imread(self.current_image)
+        self.img_plot = pyplot.imshow(img)
+        pyplot.show()
+
+    def handle_keypress(self, event):
+        if event.key == 'delete':
+            try:
+                os.remove(self.current_image)
+                self.ax.set_title('Deleted {}'.format(self.current_image))
+            except OSError as e:
+                self.ax.set_title('Unable to deleted {}:\n{}'.format(self.current_image, e))
+        elif event.key == ' ':
+            self.ax.set_title('Skipped')
+        else:
+            try:
+                dest_dir = key_to_dir[event.key]
+            except KeyError:
+                self.ax.set_title('')
+                print
+                return
+            try:
+                os.rename(self.current_image, os.path.join(dest_dir, self.current_image))
+                self.ax.set_title('Moved {} to {}.'.format(self.current_image, dest_dir))
+            except OSError as e:
+                self.ax.set_title('Unable to move {}:\n{}'.format(self.current_image, e))
+
+        # Move to next image (do-while kludge)
+        while True:
+            try:
+                self.current_image = self.image_itr.next()
+            except StopIteration:
+                print 'Done'
+                sys.exit()
+
+            self.fig.suptitle(self.current_image)
+            img = mpimg.imread(self.current_image)
+            self.img_plot.set_data(img)
+
+            pyplot.draw()
+
+            if os.path.exists(self.current_image):
+                break
+
+if __name__ == '__main__':
+    dirs = sys.argv[1].split(',')
+    keys = ['right', 'left', 'up', 'down']
+    key_to_dir = {}
+    for key, dir in zip(keys, dirs):
+        key_to_dir[key] = dir
+    if len(sys.argv) > 2:
+        files = sys.argv[2:]
+    else:
+        files = [f for f in os.listdir(os.getcwd()) if os.path.isfile(f)]
+
+    for dir in key_to_dir.values():
+        if os.path.exists(dir):
+            if not os.path.isdir(dir):
+                print '"{}" is not a directory!'
+                sys.exit(2)
+        else:
+            os.mkdir(dir)
+            print 'Created directory "{}".'
+
+    print 'Instructions:'
+    for key, classification in key_to_dir.iteritems():
+        print 'Hit {} to classify as "{}"'.format(key, classification)
+    print
+    # raw_input('Hit enter to continue')
+
+    gui = ImageClassifierGUI(key, files)

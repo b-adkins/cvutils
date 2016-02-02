64cc5027ab9b083a9f3ca7cc8db6949d8d67380c
Boone Adkins <boone.adkins@gmail.com>
2016-02-01 19:44:56 -0600

    Start of an image tagger. Places rectangles where clicked.

--- quick_classify.py
+++ quick_classify.py
@@ -1,6 +1,7 @@
 import cv2
 import matplotlib.image as mpimg
 import matplotlib.pyplot as pyplot
+import matplotlib.patches
 import numpy as np
 import os
 import sys
@@ -8,7 +9,8 @@ import sys
 
 class ImageShower(object):
     def __init__(self):
-        self.current_image = ''
+        self.current_file = ''
+        self.img = np.array([[]])
 
         self.fig, self.ax = pyplot.subplots()
 
@@ -17,17 +19,17 @@ class ImageShower(object):
         :param path: To image
         '''
 
-        img = mpimg.imread(path)
+        self.img = mpimg.imread(path)
 
         # First time
-        if not self.current_image:
-            self.img_plot = pyplot.imshow(img)
+        if not self.current_file:
+            self.img_plot = pyplot.imshow(self.img)
         else:
-            self.img_plot.set_data(img)
-        self.current_image = path
+            self.img_plot.set_data(self.img)
+        self.current_file = path
 
         self.ax.set_title(subtitle)
-        self.fig.suptitle(self.current_image)
+        self.fig.suptitle(self.current_file)
         pyplot.draw()
 
 
@@ -57,9 +59,9 @@ class SlideShow(ImageShower):
         self.update(self.images[self.i_current], self.subtitles[self.i_current])
 
 
-class ImageClassifierGUI(ImageShower):
+class ImageClassifier(ImageShower):
     def __init__(self, files, key_to_dir):
-        super(ImageClassifierGUI, self).__init__()
+        super(ImageClassifier, self).__init__()
 
         self.image_itr = files.__iter__()
         self.update(self.image_itr.next())
@@ -69,10 +71,10 @@ class ImageClassifierGUI(ImageShower):
     def handle_keypress(self, event):
         if event.key == 'delete':
             try:
-                os.remove(self.current_image)
-                self.ax.set_title('Deleted {}'.format(self.current_image))
+                os.remove(self.current_file)
+                self.ax.set_title('Deleted {}'.format(self.current_file))
             except OSError as e:
-                self.ax.set_title('Unable to deleted {}:\n{}'.format(self.current_image, e))
+                self.ax.set_title('Unable to deleted {}:\n{}'.format(self.current_file, e))
         elif event.key == ' ':
             self.ax.set_title('Skipped')
         else:
@@ -83,10 +85,10 @@ class ImageClassifierGUI(ImageShower):
                 print
                 return
             try:
-                os.rename(self.current_image, os.path.join(dest_dir, self.current_image))
-                self.ax.set_title('Moved {} to {}.'.format(self.current_image, dest_dir))
+                os.rename(self.current_file, os.path.join(dest_dir, self.current_file))
+                self.ax.set_title('Moved {} to {}.'.format(self.current_file, dest_dir))
             except OSError as e:
-                self.ax.set_title('Unable to move {}:\n{}'.format(self.current_image, e))
+                self.ax.set_title('Unable to move {}:\n{}'.format(self.current_file, e))
 
         # Move to next image (do-while kludge)
         while True:
@@ -98,6 +100,87 @@ class ImageClassifierGUI(ImageShower):
                 sys.exit()
             except (OSError, IOError):
                 continue
+
+class ImageTagger(ImageClassifier):
+    def __init__(self, files, key_to_dir):
+        super(ImageTagger, self).__init__(files, key_to_dir)
+
+        self.fig.canvas.mpl_connect('button_press_event', self.handle_click)
+
+        self.sizes = [[64, 128]]  # Possible rectangle sizes
+        self.rects = []  # Tagged rectangles
+
+    def handle_click(self, event):
+        y_max, x_max, _ = self.img.shape
+
+        print event
+        for rect in self.rects:
+             x_rect, y_rect = rect.get_xy()
+             width = rect.get_width()
+             height = rect.get_height()
+             if (x_rect <= event.xdata < x_rect + width) and (y_rect <= event.ydata < y_rect + height):
+                # Is in rectangle
+                if event.button == 3:
+                    # Remove on right click
+                    rect.remove()  # Remove from the plot
+                    self.rects.remove(rect)  # Remove from the list
+                    pyplot.draw()
+                    return
+                else:
+                    rect.set_edgecolor('g')
+                    pyplot.draw()
+                    return
+
+        size = self.sizes[0]
+        xy = [event.xdata - size[0]/2, event.ydata - size[1]/2]
+
+        # Keep in bounds
+        if xy[0] < 0:
+            xy[0] = 0
+        elif xy[0] + size[0] > x_max:
+            xy[0] = x_max - size[0]
+
+        if xy[1] < 0:
+            xy[1] = 0
+        elif xy[1] + size[1] > y_max:
+            xy[1] = y_max - size[1]
+
+        rect = matplotlib.patches.Rectangle(xy, size[0], size[1], fill=False)
+        self.rects.append(rect)
+        self.ax.add_patch(rect)
+
+        pyplot.draw()
+
+
+# if __name__ == '__main__':
+#     dirs = sys.argv[1].split(',')
+#     keys = ['right', 'left', 'up', 'down']
+#     key_to_dir = {}
+#     for key, dir in zip(keys, dirs):
+#         key_to_dir[key] = dir
+#     if len(sys.argv) > 2:
+#         files = sys.argv[2:]
+#     else:
+#         files = [f for f in os.listdir(os.getcwd()) if os.path.isfile(f)]
+#
+#     for dir in key_to_dir.values():
+#         if os.path.exists(dir):
+#             if not os.path.isdir(dir):
+#                 print '"{}" is not a directory!'
+#                 sys.exit(2)
+#         else:
+#             os.mkdir(dir)
+#             print 'Created directory "{}".'
+#
+#     print 'Instructions:'
+#     for key, classification in key_to_dir.iteritems():
+#         print 'Hit {} to classify as "{}"'.format(key, classification)
+#     print
+#     # raw_input('Hit enter to continue')
+#
+#     gui = ImageClassifier(files, key_to_dir)
+#
+#     pyplot.show()
 
 if __name__ == '__main__':
     dirs = sys.argv[1].split(',')
@@ -125,6 +208,6 @@ if __name__ == '__main__':
     print
     # raw_input('Hit enter to continue')
 
-    gui = ImageClassifierGUI(files, key_to_dir)
+    gui = ImageTagger(files, key_to_dir)
 
     pyplot.show()

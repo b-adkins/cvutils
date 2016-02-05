77fe735ada99ed7abf2a5dd83c8e4b5fcd3fc849
Boone Adkins <boone.adkins@gmail.com>
2016-02-05 08:50:32 -0600

    Added a GPL license. This license is is intended to cover all previous versions in Git.

--- quick_classify.py
+++ quick_classify.py
@@ -1,3 +1,21 @@
+# The following file contains open-source, non-Matlab GUIs to view images and
+# crop them for training data.
+#
+# Copyright (C) 2016 Boone Adkins
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 import collections
 import matplotlib.image as mpimg
 import matplotlib.pyplot as pyplot

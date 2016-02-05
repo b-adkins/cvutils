e8bfbbeb3dcb001717ae5dcff0f72fc39e550260
Boone Adkins <boone.adkins@gmail.com>
2016-02-05 09:02:39 -0600

    Drag tagger's dragged rectangle.

--- quick_classify.py
+++ quick_classify.py
@@ -18,8 +18,9 @@
 
 import collections
 import matplotlib.image as mpimg
+import matplotlib.lines as mlines
 import matplotlib.pyplot as pyplot
-import matplotlib.patches
+import matplotlib.patches as mpatches
 import numpy as np
 import os
 import PIL.Image
@@ -137,12 +138,12 @@ class ImageTagger(ImageClassifier):
         self.rect_being_dragged = False
 
         self.colors = ['b', 'purple', 'r', 'g', 'y', 'c', 'teal']  # Color choices for rectangles
+        self.rect_options = {'fill': False, 'color': self.colors[0], 'linewidth': 3}
 
         self.sizes = [[64, 128]]  # Possible rectangle sizes
         self.rects = []  # Tagged rectangles
         self.rect_colors = {}  # Matplotlib Patch has no "get_color()"; thus must be tracked separately
         self.color_to_dir = {color: dir for dir, color in zip(self.key_to_dir.values(), self.colors)}
-
 
     def get_rect_at(self, x, y):
         '''
@@ -159,17 +160,23 @@ class ImageTagger(ImageClassifier):
                 return rect
         return None
 
+    def crop_rectangle(self, rect):
+        x, y = rect.get_xy()
+        width = rect.get_width()
+        height = rect.get_height()
+
+        # Crop each rectangle's image
+        img_array = self.img[y:y+height, x:x+width]
+        img_array = (img_array*255).astype(np.uint8)  # Convert from 0.0 to 1.0 to 0 to 255
+
+        return img_array
+
     def save_rectangles(self):
         outfiles = []
-        for i, rect in enumerate(self.rects):
-            x, y = rect.get_xy()
-            width = rect.get_width()
-            height = rect.get_height()
-
-            # Crop each rectangle's image
-            img_array = self.img[y:y+height, x:x+width]
-            img_array = (img_array*255).astype(np.uint8)  # Convert from 0.0 to 1.0 to 0 to 255
-
+               
+        for i, rect in enumerate(self.rects): 
+            img_array = self.crop_rectangle(rect)
+            
             # Create output path
             file, ext = os.path.splitext(self.current_file)
             ext = ext[1:]  # Remove the leading '.'
@@ -275,43 +282,137 @@ class ImageTagger(ImageClassifier):
                 elif xy[1] + size[1] > y_max:
                     xy[1] = y_max - size[1]
 
-                color = self.colors[0]
-                new_rect = matplotlib.patches.Rectangle(xy, size[0], size[1], fill=False, color=color, linewidth='3')
+                new_rect = mpatches.Rectangle(xy, size[0], size[1], **self.rect_options)
                 self.rects.append(new_rect)
-                self.rect_colors[new_rect] = color
+                self.rect_colors[new_rect] = self.colors[0]
                 self.ax.add_patch(new_rect)
                 pyplot.draw()
 
+# Drag for scale
+class ImageDragTagger(ImageTagger):
+    def __init__(self, files, key_to_dir, aspect_ratio=0.5, margin=(0.25, 0.125)):
+        '''
 
-# if __name__ == '__main__':
-#     dirs = sys.argv[1].split(',')
-#     keys = ['right', 'left', 'up', 'down']
-#     key_to_dir = {}
-#     for key, dir in zip(keys, dirs):
-#         key_to_dir[key] = dir
-#     if len(sys.argv) > 2:
-#         files = sys.argv[2:]
-#     else:
-#         files = [f for f in os.listdir(os.getcwd()) if os.path.isfile(f)]
+        :param files:
+        :param key_to_dir:
+        :param aspect_ratio: width/height
+        :param margin:
+        :return:
+        '''
+        super(ImageDragTagger, self).__init__(files,key_to_dir)
+
+        self.pct_margin = 0.25  # 16px out of 64x128 in the original HOG paper
+
+        self.aspect_ratio = float(self.sizes[0][0])/float(self.sizes[0][1])
+        self.is_sizing_rect = False
+
+    def handle_mouse_press(self, event):
+        if event.button == 1:
+            clicked_rect = self.get_rect_at(event.xdata, event.ydata)
+            if clicked_rect:
+                self.rect_being_dragged = clicked_rect
+            else:
+                self.is_sizing_rect = True
+#                self.sizing_circle = mpatches.Circle(event.xdata, event.ydata, 0.1, ec="none")
+                ep = 1
+                # Or fancy patch box?
+                self.x0, self.y0 = event.xdata, event.ydata  # Where the drag was started
+                self.rect_corner = (self.x0, self.y0)  # Offset the rectangle slightly
+                # self.sizing_line = mlines.Line2D(self.x0 + np.array([0, ep]), self.y0 + np.array([0, ep]), lw=4)
+                self.rect_being_created = mpatches.Rectangle((self.x0, self.y0), ep, ep, **self.rect_options)
+                self.rects.append(self.rect_being_created)  # As its being made, it can be removed or changed category=
+                self.ax.add_patch(self.rect_being_created)
+                # self.ax.add_patch(self.sizing_line)
+                pyplot.draw()
+
+    def handle_mouse_motion(self, event):
+        print event
+        x_mouse, y_mouse = event.xdata, event.ydata
+        if x_mouse is None or y_mouse is None:
+            print 'event.xdata or event.ydata is None!'
+            return
+        if self.is_sizing_rect == True:
+            h = y_mouse - self.y0
+            w = int(np.abs(h)*self.aspect_ratio)
+
+            self.rect_being_created.set_x(self.x0 - w/2)  # Recenter the top
+            self.rect_being_created.set_width(w)
+            self.rect_being_created.set_height(h)
+            pyplot.draw()
+        else:
+            super(ImageDragTagger, self).handle_mouse_motion(event)
+
+    def handle_mouse_release(self, event):
+        if event.button == 1:
+
+            if self.is_sizing_rect:
+                self.is_sizing_rect = False
+#                self.sizing_line.remove()
+                pyplot.draw()
+            return  # Avoid creating new rectangles on click
+
+        super(ImageDragTagger, self).handle_mouse_release(event)
+
+# =======
 #
-#     for dir in key_to_dir.values():
-#         if os.path.exists(dir):
-#             if not os.path.isdir(dir):
-#                 print '"{}" is not a directory!'
-#                 sys.exit(2)
+#         :param files:
+#         :param key_to_dir:
+#         :param aspect_ratio: width/height
+#         :param margin:
+#         :return:
+#         '''
+#         super(ImageDragTagger, self).__init__(files,key_to_dir)
+#
+#         self.pct_margin = 0.25  # 16px out of 64x128 in the original HOG paper
+#
+#         self.aspect_ratio = float(self.sizes[0][0])/float(self.sizes[0][1])
+#         self.is_sizing_rect = False
+#
+#     def handle_mouse_press(self, event):
+#         if event.button == 1:
+#             clicked_rect = self.get_rect_at(event.xdata, event.ydata)
+#             if clicked_rect:
+#                 self.rect_being_dragged = clicked_rect
+#             else:
+#                 self.is_sizing_rect = True
+# #                self.sizing_circle = mpatches.Circle(event.xdata, event.ydata, 0.1, ec="none")
+#                 ep = 1
+#                 # Or fancy patch box?
+#                 self.x0, self.y0 = event.xdata, event.ydata  # Where the drag was started
+#                 self.sizing_line = mlines.Line2D(self.x0 + np.array([0, ep]), self.y0 + np.array([0, ep]), lw=4)
+#                 self.rect_being_created = mpatches.Rectangle((self.x0, self.y0), ep, ep, **self.rect_options)
+#                 self.rects.append(self.rect_being_created)  # As its being made, it can be removed or changed category=
+#                 self.ax.add_patch(self.rect_being_created)
+#                 pyplot.draw()
+#
+#     def handle_mouse_motion(self, event):
+#         print event
+#         x_mouse, y_mouse = event.xdata, event.ydata
+#         if x_mouse is None or y_mouse is None:
+#             print 'event.xdata or event.ydata is None!'
+#             return
+#         if self.is_sizing_rect == True:
+#             h = y_mouse - self.y0
+#             w = int(np.abs(h)*self.aspect_ratio)
+#
+#             self.rect_being_created.set_x(self.x0 - w/2)  # Recenter the top
+#             self.rect_being_created.set_width(w)
+#             self.rect_being_created.set_height(h)
+#             pyplot.draw()
 #         else:
-#             os.mkdir(dir)
-#             print 'Created directory "{}".'
+#             super(ImageDragTagger, self).handle_mouse_motion(event)
 #
-#     print 'Instructions:'
-#     for key, classification in key_to_dir.iteritems():
-#         print 'Hit {} to classify as "{}"'.format(key, classification)
-#     print
-#     # raw_input('Hit enter to continue')
+#     def handle_mouse_release(self, event):
+#         # Don't create new rectangles on click
+#         if self.is_sizing_rect:
+#             self.is_sizing_rect = False
+#         else:
+#             super(ImageDragTagger, self).handle_mouse_release(event)
 #
-#     gui = ImageClassifier(files, key_to_dir)
-#
-#     pyplot.show()
+# >>>>>>> Stashed changes
+    def save_rectangles(self):
+        # Cut them out, then scale them
+        pass
 
 if __name__ == '__main__':
     dirs = sys.argv[1].split(',')
@@ -339,6 +440,6 @@ if __name__ == '__main__':
     print
     # raw_input('Hit enter to continue')
 
-    gui = ImageTagger(files, key_to_dir)
+    gui = ImageDragTagger(files, key_to_dir)
 
     pyplot.show()
